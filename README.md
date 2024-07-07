d8da53c48554e8ac34b11d915adf7ba86e34848a
ANNGLORIOUS <mueniannglorious@gmail.com>
2024-07-07 12:37:57 +0300

    initial commit

--- README.md
+++ README.md
@@ -49,7 +49,7 @@ The challenge reinforces understanding of event listeners and their use in creat
 * A github account
 
 1. Open your terminal and go to the directory you wish to work from.
-2. Go to the following url using ur github account https://github.com/ANNGLORIOUS/code-1
+2. Go to the following url using ur github account https://github.com/ANNGLORIOUS/code-2
 3. Go to the code tab and clone the ssh key
 4. Go back to the termina and type git clone <-followed by the ssh key you copied /cloned ->
 5. Enter your new cloned repository and type in code .
--- shopping.js
+++ shopping.js
@@ -1,5 +1,73 @@
-// selectors
-const inputText =document.getElementById('input-text');
+// Array to store shopping list items
+let shoppingList = JSON.parse(localStorage.getItem('shoppingList')) || [];
+
+// DOM elements
+const inputText = document.getElementById('input-text');
 const addItemBtn = document.getElementById('add-item-btn');
-const clearBtn = document.getElementById('clearBtn');
-    const shoppingList = document.getElementById('shoppingList');
+const clearBtn = document.getElementById('clear-btn');
+const shoppingListContainer = document.getElementById('shopping-list');
+
+// Function to render shopping list
+function renderShoppingList() {
+    shoppingListContainer.innerHTML = '';
+    shoppingList.forEach((item, index) => {
+        const li = document.createElement('li');
+        li.textContent = item;
+        li.dataset.index = index;
+        if (item.purchased) {
+            li.classList.add('purchased');
+        }
+        li.addEventListener('click', markPurchased);
+        li.addEventListener('dblclick', editItem);
+        shoppingListContainer.appendChild(li);
+    });
+}
+
+// Function to add item to shopping list
+function addItem() {
+    const item = inputText.value.trim();
+    if (item) {
+        shoppingList.push({ text: item, purchased: false });
+        inputText.value = '';
+        saveToLocalStorage();
+        renderShoppingList();
+    }
+}
+
+// Function to mark item as purchased
+function markPurchased(event) {
+    const index = parseInt(event.target.dataset.index);
+    shoppingList[index].purchased = !shoppingList[index].purchased;
+    saveToLocalStorage();
+    renderShoppingList();
+}
+
+// Function to edit item
+function editItem(event) {
+    const index = parseInt(event.target.dataset.index);
+    const newText = prompt('Edit item:', shoppingList[index].text);
+    if (newText) {
+        shoppingList[index].text = newText;
+        saveToLocalStorage();
+        renderShoppingList();
+    }
+}
+
+// Function to clear shopping list
+function clearList() {
+    shoppingList = [];
+    saveToLocalStorage();
+    renderShoppingList();
+}
+
+// Function to save shopping list to local storage
+function saveToLocalStorage() {
+    localStorage.setItem('shoppingList', JSON.stringify(shoppingList));
+}
+
+// Event listeners
+addItemBtn.addEventListener('click', addItem);
+clearBtn.addEventListener('click', clearList);
+
+// Render shopping list on page load
+renderShoppingList();
